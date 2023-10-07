f4bee53305305ed6676a7ee5aff2f4558ebacf12
Junseok Lee <jslee435@gmail.com>
2023-10-07 19:19:50 +0900

    update script

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-#! bin/bash
-./build/bin/webacachesim_cli  wiki2018_test.tr LRU 1024000 --segment_window=4000000 --dburi="mongodb+srv://jslee95:tanggu3275@cluster0.xlzzkiy.mongodb.net/Default"
+#!/bin/bash
+./build/bin/webcachesim_cli wiki2018_test.tr LRB 614400 --segment_window=1000000 --memory_window=614400 --dburi="mongodb+srv://jslee95:tanggu3275@cluster0.xlzzkiy.mongodb.net/Default"
+./build/bin/webcachesim_cli wiki2018_test.tr LRB 614400 --segment_window=1000000 --memory_window=614400 --dburi="mongodb+srv://jslee95:<password>@cluster0.xlzzkiy.mongodb.net/?retryWrites=true&w=majority/Default"
